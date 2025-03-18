be689233f43fe640e73a9111bb9f2515cc49dc49
SunnyFlops <arav.prasad@outlook.com>
2025-03-19 11:15:48 +1300

    changes to css handling, html optimisations
    thanks cooki

--- global.js
+++ global.js
@@ -1,10 +1,10 @@
 const html = document.querySelector("html");
 
 window.onload = () => {
-    html.style.backgroundColor = '#6b5fff';
+    html.style.backgroundColor = "#ffffff";
 }
 
-window.addEventListener("scroll", () => {
+function switchBG() {
     const halfwayPoint = document.documentElement.scrollHeight / 2;
 
     if (window.scrollY + window.innerHeight / 2 < halfwayPoint) {
@@ -12,7 +12,12 @@ window.addEventListener("scroll", () => {
     } else {
         html.style.backgroundColor = '#6b5fff';
     }
-})
+}
+
+window.addEventListener("scroll", switchBG);
+window.addEventListener("wheel", switchBG);
+window.addEventListener("load", switchBG);
+window.addEventListener("resize", switchBG);
 
 function topFunction() {
     window.scrollTo({ top: 0, behavior: "smooth" });
